d3d70b5e02ff3c41cb48c35975db91a426308cdf
akvist <111497667+akvist@users.noreply.github.com>
2022-12-21 20:00:35 +0100

    Hotell app frontend

---
+++ README.md
@@ -0,0 +1,133 @@
+# Hotell app, front-end
+
+This is the front-end of an app created with React.js.
+The back-end can be found at: https://github.com/akvist/Hotel-app-backend.git
+
+Deployment: https://coruscating-ganache-ede5f1.netlify.app
+
+## Table of contents
+
+- Introduction
+- Scope of Functionalities
+- Deployment
+- Demo
+- Lessons Learned
+- Tech Stack
+- Run Locally Together with Back-end Code
+- Links
+
+## Introduction
+
+This is a front-end for a full-stack project. The application was made to learn the whole process of developing an app, from writing both back-and front-end code with Node.js and React.js to using services as git, databases, and deployment services.
+
+The application is a simulation of the functionality of three elevators in a hotell with ten floors.
+
+## Scope of Functionalities
+
+The project is a simulation of a hotell with ten floors and three elevators.
+It consists of ten buttons, one for each floor, and the three elevators that move between
+these ten floors when the buttons are pushed.
+
+The starting point of the elevators is that they are standing still. When pushing a button,
+the closest elevator will leave it's location and move towards the chosen floor with a pace
+of 2 seconds per floor and stop when it has arrived. If a botton is pushed while one or more elevators
+are moving, the closest free elevator will be called to the chosen floor. All three elevators
+can move at the same time. If a button is pushed while all three elevators are moving, the
+elevators will still finish with their current paths. The elevattor that arrives first to its
+destination will after its arrival start again and move to the chosen floor. If a button is pushed and an elevator
+already is at the chosen floor, nothing will happen.
+
+## Deployment
+
+The whole full-stack project has been deployed and can be found at: https://coruscating-ganache-ede5f1.netlify.app
+
+## Demo
+
+Demo video on youtube: https://www.youtube.com/watch?v=VU5xx1ZXCaE
+
+## Lessons Learned
+
+**Lessons learned and highlights of this code**:
+
+- Managing Hooks such as useState and useEffect
+- Handling events such as onClick with a custom function that takes variables
+- Managing git and GitHub
+- Writing DRY code
+  - By dynamically rendering multiple elevatorbuttons from an array using the map function (in the return-function in App.js)
+  - By dynamically rendering multiple squares that change colors to simulate the movements of the elevators using the map function (in the return-function in App.js)
+- Fetching data from back-end code
+- Styling with CSS
+- The logic that checks the elevators' status every second and sets it to the current status
+- The logic that decides which color the squares that simulate the elevators' movements will be
+
+**Together with back-end code**:
+
+- Making a full-stack application
+
+## Tech Stack
+
+**Server:** React.js
+
+**Deployment:** Netlify
+
+## Run Locally Together with Back-end Code
+
+**Front-end**
+
+Clone the front-end
+
+```bash
+  git clone https://github.com/akvist/Hotel-app-frontend.git
+```
+
+Go to the project directory
+
+```bash
+  cd Hotellapp-frontend
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm start
+```
+
+**Back-end**
+
+Clone the back-end
+
+```bash
+  git clone https://github.com/akvist/Hote-lapp-backend.git
+```
+
+Go to the project directory
+
+```bash
+  cd Hotellapp-backend
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm start
+```
+
+**Important**: Environmental variables are needed for the Back-end, as mentioned in that README. Use the PORT 5000 for the endpoints since they are set to 5000 in the front-end code.
+
+## 🔗 Links
+
+You can find me on LinkedIn:
+
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/amira-kvist-7a5083187/)
---
+++ src/App.js
@@ -0,0 +1,125 @@
+import React, { useEffect, useState } from 'react';
+import './index.css';
+
+export const App = () => {
+
+  const elevatorButtons = [
+    {row: 1, column1: 1, column2: 2}, 
+    {row: 2, column1: 3, column2: 4}, 
+    {row: 3, column1: 5, column2: 6}, 
+    {row: 4, column1: 7, column2: 8}, 
+    {row: 5, column1: 9, column2: 10}];
+
+  const floors = [
+    {id: 1, floor: 10}, 
+    {id: 2, floor: 9}, 
+    {id: 3, floor: 8}, 
+    {id: 4, floor: 7}, 
+    {id: 5, floor: 6}, 
+    {id: 6, floor: 5}, 
+    {id: 7, floor: 4}, 
+    {id: 8, floor: 3}, 
+    {id: 9, floor: 2}, 
+    {id: 10, floor: 1} 
+  ];
+
+  const moving1 = []; const moving2 = []; const moving3 = [];
+  const initialState = [
+    {id: 1, floor: "...", status: "Elevator is standing still"},
+    {id: 2, floor: "...", status: "Elevator is standing still"},
+    {id: 3, floor: "...", status: "Elevator is standing still"} 
+  ];
+
+  const [elevators, setStatus] = useState(initialState);
+  const [movingElevator1, setGoing1] = useState(moving1);
+  const [movingElevator2, setGoing2] = useState(moving2);
+  const [movingElevator3, setGoing3] = useState(moving3);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      //fetch("https://tenfloorshotell.herokuapp.com/api/elevatorstatus")
+      fetch("https://hotellapp.fly.dev/api/elevatorstatus")
+      .then(response => response.json())
+      .then(data => {
+        setStatus(current =>
+          current.map(obj => {
+            if (data.elevatorStatus[3] === "Elevator is running") {setGoing1([])};
+            if (data.elevatorStatus[4] === "Elevator is running") {setGoing2([])};
+            if (data.elevatorStatus[5] === "Elevator is running") {setGoing3([])};
+            if (obj.id === 1) {return {...obj, floor: data.elevatorStatus[0], status: data.elevatorStatus[3]};}
+            if (obj.id === 2) {return {...obj, floor: data.elevatorStatus[1], status: data.elevatorStatus[4]};}
+            if (obj.id === 3) {return {...obj, floor: data.elevatorStatus[2], status: data.elevatorStatus[5]};}
+            return obj; 
+          }));
+      })
+      .catch(err => console.error(err));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  async function handleClick(floor) {
+    //await fetch(`https://tenfloorshotell.herokuapp.com/api/callelevatorto/${floor}`)
+    await fetch(`https://hotellapp.fly.dev/api/callelevatorto/${floor}`)
+    .then(res => res.json())
+    .then(data => {
+      if (data === 1) {setGoing1(data)};
+      if (data === 2) {setGoing2(data)};
+      if (data === 3) {setGoing3(data)};
+    })
+    .catch(err => console.error(err));
+  };
+
+
+  function renderSquare1(floor) {
+    if (elevators[0].status === "Elevator is standing still" && movingElevator1 === 1 && floor.floor === elevators[0].floor) {return <span className="button squarebuttongo"></span>;}
+    else if (elevators[0].status === "Elevator is standing still" && movingElevator1 !== 1 && floor.floor === elevators[0].floor) {return <span className="button squarebuttonstill"></span>;}
+    else if ((elevators[0].status === "Elevator is running" && floor.floor === elevators[0].floor)) {return <span className="button squarebuttongoing"></span>;}
+    else {return (<span className="button squarebutton"></span>)}
+  }
+
+  function renderSquare2(floor) {
+    if (elevators[1].status === "Elevator is standing still" && movingElevator2 === 2 && floor.floor === elevators[1].floor) {return <span className="button squarebuttongo"></span>;}
+    else if (elevators[1].status === "Elevator is standing still" && movingElevator2 !== 2 && floor.floor === elevators[1].floor) {return <span className="button squarebuttonstill"></span>;}
+    else if (elevators[1].status === "Elevator is running" && floor.floor === elevators[1].floor) {return <span className="button squarebuttongoing"></span>;}
+    else {return (<span className="button squarebutton"></span>)}
+  }
+
+  function renderSquare3(floor) {
+    if (elevators[2].status === "Elevator is standing still" && movingElevator3 === 3 && floor.floor === elevators[2].floor) {return <span className="button squarebuttongo"></span>;}
+    else if (elevators[2].status === "Elevator is standing still" && movingElevator3 !== 3 && floor.floor === elevators[2].floor) {return <span className="button squarebuttonstill"></span>;}
+    else if ((elevators[2].status === "Elevator is running" && floor.floor === elevators[2].floor)) {return <span className="button squarebuttongoing"></span>;}
+    else {return (<span className="button squarebutton"></span>)}
+  }
+
+
+
+  return (
+    <div className="flex-container">
+
+      <div className="flex-child-left">
+        { elevatorButtons.map(elevatorButton => {
+          return (
+            <div key={elevatorButton.row} >
+              <button className="button roundbutton" onClick={() => handleClick(elevatorButton.column1)}>{elevatorButton.column1}</button>
+              <button className="button roundbutton" onClick={() => handleClick(elevatorButton.column2)}>{elevatorButton.column2}</button>
+            </div>
+          );})}
+      </div> 
+
+      <div className="flex-child-right">
+        <span> 1 2 3 </span> <div></div>
+        { floors.map(floor => {
+          return (
+            <div key={floor.id}>
+              <span>{renderSquare1(floor)}</span><span>{renderSquare2(floor)}</span><span>{renderSquare3(floor)}</span>
+              </div>
+          );})}
+      </div>
+    
+    </div>
+  )
+}
+
+
+
+//export const App = () => {};
